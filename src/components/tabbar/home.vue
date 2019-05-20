7230d8e5ca3dd82da7f83b868896835e4ab99295
SouthVillage <***@sina.com>
2019-05-20 08:23:15 +0800

    home page

---
+++ src/components/tabbar/home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <mt-swipe :auto="4000">
+      <mt-swipe-item v-for="item in list" :key:''>
+        <img :src="item.src" alt="">
+      </mt-swipe-item>
+    </mt-swipe>
+  </div>
+</template>
+
+<script>
+  export default{
+    data(){
+      return{
+        // 向外面暴露数据
+        list:[]   
+      }
+    },
+    created(){
+      getlunbotu()
+    },
+    methods:{
+      getlunbotu(){
+        this.$http.get('http://www.').then(result=>{
+          console.log(result);
+          list = result.img;
+        })
+      }
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+.mint-swipe {
+    height:200px;
+  .mint-swipe-item {
+    &:nth-child(1){
+      background-color: red;
+    }
+    &:nth-child(2){
+      background-color: yellow;
+    }
+    &:nth-child(3){
+      background-color: blue;
+    }
+  }
+}
+</style>
+
--- src/main.js
+++ src/main.js
@@ -8,10 +8,28 @@ import app from './App.vue'
 //导入Mui CSS文件
 import './lib/mui/css/mui.css'
 
-
+//导入 mui  css扩展图标文件
+import './lib/mui/css/icons-extra.css'
 Vue.component(Header.name,Header);
+
+//引入路由
+import vueRouter from 'vue-router'
+Vue.use(vueRouter)
+import router from './router.js'
+
+//轮播图
+import { Swipe, SwipeItem } from 'mint-ui';
+
+
+//vue-resource
+import Vueresource from 'vue-resource'
+Vue.use(Vueresource)
+
+Vue.component(Swipe.name, Swipe);
+Vue.component(SwipeItem.name, SwipeItem);
 
 var vm = new Vue({
   el:'#app',
-  render:c=>c(app)
+  render:c=>c(app),
+  router
 })
